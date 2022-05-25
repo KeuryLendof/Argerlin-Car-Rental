55f920d0ca4c6674b8c09c5e3e809c12c3b9e67b
KeuryLendof <lendofkeury@gmail.com>
2022-05-25 16:28:01 -0400

    header 80%

--- src/Componentes/Header.js
+++ src/Componentes/Header.js
@@ -1,42 +1,22 @@
-import { Component } from 'react'
 import './style.css'
 
-class Header extends Component{
+function Header(){
 
-    componentDidMount(){
-        const toggleButton = document.getElementsByClassName('toggle-button')[0]
-        const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+  
 
-        toggleButton.addEventListener('click', () => {
-            navbarLinks.classList.toggle('active')
-            console.log('funciono')
-        })
-    }
-
-
-    render(){
-
-        return(
-            <header className='navContainer'>
-                <a href='/'>
-                    <h1 className="brand-title">ApiCrypto</h1>
-                </a>
-                <p className="toggle-button">
-                    <span className="bar"></span>
-                    <span className="bar"></span>
-                    <span className="bar"></span>
-                </p>
-                <div className="navbar-links">
-                    <ul>
-                        <li><a href="#top">Top100</a></li>
-                        <li><a href="#search">Search</a></li>
-                        <li><a href="#footer">Contact</a></li>
-                    </ul>
-                </div>
-            </header>
-            
-        )
-    }
+    return(
+        <header className="header">  
+            <a href="/" className="logo">ARGERLIN</a> 
+            <input className="menu-btn" type="checkbox" id="menu-btn" />  
+            <label className="menu-icon" for="menu-btn"><span className="navicon"></span></label>  
+            <ul className="menu">  
+                <li><a href="#about">Buscar</a></li>  
+                <li><a href="#careers">Ubicacion</a></li>  
+                <li><a href="#contact">Acerca de</a></li>  
+            </ul>  
+        </header>  
+        
+    )
     
 }
 
