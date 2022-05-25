12ffeca51bc455ef0085ee32e0fd9acb06b8aff3
KeuryLendof <lendofkeury@gmail.com>
2022-05-24 21:38:13 -0400

    Cambio de header a banner

---
+++ src/Componentes/Banner.jsx
@@ -0,0 +1,14 @@
+import './style.css'
+
+
+function Banner(){
+
+
+    return(
+        <div className="contenedorBanner">
+            <img src={require('../assets/images/Rent a car.png')} className="banner-principal" alt="" />
+        </div>
+    )
+}
+
+export default Banner;
--- src/Componentes/Header.jsx
+++ src/Componentes/Header.jsx
@@ -1,12 +1,11 @@
 import './style.css'
 
-
 function Header(){
 
 
     return(
-        <div className="contenedorHeader">
-            <img src={require('../assets/images/Rent a car.png')} className="banner-principal" alt="" />
+        <div>
+
         </div>
     )
 }
