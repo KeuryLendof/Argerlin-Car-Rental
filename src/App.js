4bac47eb973c61e6a14bba7a3275a398f26e81c5
KeuryLendof <lendofkeury@gmail.com>
2022-05-26 11:30:31 -0400

    maqueta para las marcas

--- src/App.js
+++ src/App.js
@@ -25,10 +25,28 @@ function App() {
       <Banner/>
       <div className="join" id="search">
         <div className="join_container">
-          <h2 className="join_title">Busca tu vehiculo preferido aqui!</h2>
+          <h2 className="join_title">¿Que quieres rental?</h2>
           <form action="" className="join_form">
             <input type="text" placeholder="example 'mercedes' " className="join_input" onChange={e => setBuscar(e.target.value)} />
           </form>
+          <section className='marcas'>
+            <article>
+              <img src='' alt='mercedes'/>
+              <p>Mercedes</p>
+            </article>
+            <article>
+              <img src='' alt='honda'/>
+              <p>Honda</p>
+            </article>
+            <article>
+              <img src='' alt='hyundai'/>
+              <p>Hyundai</p>
+            </article>
+            <article>
+              <img src='' alt='kia'/>
+              <p>Kia</p>
+            </article>
+          </section>
         </div>
       </div>
       <Cards vehiculos={data}/>
---
+++ src/Componentes/Marcas.jsx
@@ -0,0 +1,15 @@
+import './style.css'
+
+
+function Marcas(){
+
+
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Marcas;
