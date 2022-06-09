53412e0dc916146b7ede0ed51a5f60edef02f103
KeuryLendof <lendofkeury@gmail.com>
2022-06-09 10:52:00 -0700

    experimentando con slider y cards

--- src/Componentes/Slider.jsx
+++ src/Componentes/Slider.jsx
@@ -14,46 +14,181 @@ import { Pagination, Navigation } from "swiper";
 
 function Slider(){
 
-    return(
-        <>
+  return(
+    <>
       <div className="containerSlider">
-      <Swiper
-        slidesPerView={1}
-        spaceBetween={10}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        breakpoints={{
-          640: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-          768: {
-            slidesPerView: 4,
-            spaceBetween: 40,
-          },
-          1024: {
-            slidesPerView: 5,
-            spaceBetween: 50,
-          },
-        }}
-        modules={[Pagination, Navigation]}
-        className="mySwiper"
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={10}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 4,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 5,
+              spaceBetween: 50,
+            },
+          }}
+          modules={[Pagination, Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <div class="card">
+              <div class="image-content">
+                <span class="overlay"></span>
+
+                <div class="card-image">
+                  <img src="" alt="" class="card-img"/>
+                </div>
+              </div>
+
+              <div class="card-content">
+                <h2 class="name">David Dell</h2>
+                <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+
+                <button class="button">View More</button>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div class="card">
+              <div class="image-content">
+                <span class="overlay"></span>
+
+                <div class="card-image">
+                  <img src="" alt="" class="card-img"/>
+                </div>
+              </div>
+
+              <div class="card-content">
+                <h2 class="name">David Dell</h2>
+                <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+
+                <button class="button">View More</button>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div class="card">
+              <div class="image-content">
+                <span class="overlay"></span>
+
+                <div class="card-image">
+                  <img src="" alt="" class="card-img"/>
+                </div>
+              </div>
+
+              <div class="card-content">
+                <h2 class="name">David Dell</h2>
+                <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+
+                <button class="button">View More</button>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div class="card">
+              <div class="image-content">
+                <span class="overlay"></span>
+
+                <div class="card-image">
+                  <img src="" alt="" class="card-img"/>
+                </div>
+              </div>
+
+              <div class="card-content">
+                <h2 class="name">David Dell</h2>
+                <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+
+                <button class="button">View More</button>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div class="card">
+              <div class="image-content">
+                <span class="overlay"></span>
+
+                <div class="card-image">
+                  <img src="" alt="" class="card-img"/>
+                </div>
+              </div>
+
+              <div class="card-content">
+                <h2 class="name">David Dell</h2>
+                <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+
+                <button class="button">View More</button>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div class="card">
+              <div class="image-content">
+                <span class="overlay"></span>
+
+                <div class="card-image">
+                  <img src="" alt="" class="card-img"/>
+                </div>
+              </div>
+
+              <div class="card-content">
+                <h2 class="name">David Dell</h2>
+                <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+
+                <button class="button">View More</button>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div class="card">
+              <div class="image-content">
+                <span class="overlay"></span>
+
+                <div class="card-image">
+                  <img src="" alt="" class="card-img"/>
+                </div>
+              </div>
+
+              <div class="card-content">
+                <h2 class="name">David Dell</h2>
+                <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+
+                <button class="button">View More</button>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div class="card">
+              <div class="image-content">
+                <span class="overlay"></span>
+
+                <div class="card-image">
+                  <img src="" alt="" class="card-img"/>
+                </div>
+              </div>
+
+              <div class="card-content">
+                <h2 class="name">David Dell</h2>
+                <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+
+                <button class="button">View More</button>
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
       </div>
     </>
-    )
+  )
 }
 
 export default Slider;
