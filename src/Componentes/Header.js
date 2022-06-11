41530747b9f2e7d577cab54fa4d6598ae502b1b0
KeuryLendof <lendofkeury@gmail.com>
2022-06-11 17:21:51 -0400

    estilo en input busccar

--- src/Componentes/Header.js
+++ src/Componentes/Header.js
@@ -1,8 +1,12 @@
+import React from 'react';
+import { TextInput, TextInputProps, ActionIcon, useMantineTheme } from '@mantine/core';
+import { Search, ArrowRight, ArrowLeft } from 'tabler-icons-react';
 import './style.css'
 
 function Header(){
 
-  
+    const theme = useMantineTheme();
+
 
     return(
         <header className="header">  
@@ -14,8 +18,20 @@ function Header(){
                     <a class="dropdown-toggle" href="#" id="DropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Buscar
                     </a>
-                    <ul class="dropdown-menu" aria-labelledby="DropdownMenuLink">
-                        <input class="dropdown-item"/>
+                    <ul class="dropdown-menu">
+                        <TextInput
+                            icon={<Search size={18} />}
+                            radius="xl"
+                            size="md"
+                            rightSection={
+                                <ActionIcon size={32} radius="xl" color={theme.primaryColor} variant="filled">
+                                {theme.dir === 'ltr' ? <ArrowRight size={18} /> : <ArrowLeft size={18} />}
+                                </ActionIcon>
+                            }
+                            class="dropdown-item"
+                            placeholder="Search questions"
+                            rightSectionWidth={42}
+                        />
                     </ul>
                 </li>
                 <li className='itemLi'><a href="#careers">Ubicacion</a></li>  
